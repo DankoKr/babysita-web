b50a91766071435c37697d253550ad8f8cd6e141
Danko Kralski <d.kralski@student.fontys.nl>
2023-11-20 13:53:04 +0100

    Image feature

--- src/components/AccountBox.jsx
+++ src/components/AccountBox.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import useDeleteRequest from "../services/useDeleteRequest";
 import styles from "./AccountBox.module.css";
 import Button from "./Button";
@@ -12,6 +12,10 @@ const AccountBox = ({ user }) => {
   const token = TokenManager.getAccessToken();
   const deleteAccount = useDeleteRequest("/users", token);
   const patchData = usePatchRequest("/users", token);
+  const [profileImage, setProfileImage] = useState(
+    user.profileImage ||
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.MXYD9n1S0PS6KSlP401QaQHaHa%26pid%3DApi&f=1&ipt=9bc37256346704220a54e0ab7080dbea132155af57ecc97660d331dc704bfaca&ipo=images"
+  );
 
   const fields = [
     { label: "First Name", key: "firstName", value: user.firstName },
@@ -38,15 +42,44 @@ const AccountBox = ({ user }) => {
     await patchData(user.id, updatedData);
   };
 
+  const handleImageUpload = async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = async () => {
+        setProfileImage(reader.result);
+        const updatedData = {
+          profileImage: reader.result,
+        };
+        await patchData(user.id, updatedData);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className={styles.accountBox}>
       <div className={styles.accountCard}>
         <div className={styles.image}>
           <button className={styles.btn}>
-            <img src="https://i.imgur.com/wvxPV9S.png" alt="User profile" />
+            <img src={profileImage} alt="User profile image" />
           </button>
+
+          <div className={styles.fileUploadBtn}>
+            <label htmlFor="file-upload" className={styles.customFileBtn}>
+              Change Image
+            </label>
+            <input
+              id="file-upload"
+              type="file"
+              onChange={handleImageUpload}
+              className={styles.fileInput}
+            />
+          </div>
+
           <span className={styles.name}>{user.username}</span>
           <span className={styles.email}>{user.email}</span>
+
           <div className={styles.text}>
             {fields.map((field) => (
               <div key={field.label}>
@@ -59,8 +92,9 @@ const AccountBox = ({ user }) => {
               </div>
             ))}
           </div>
+
           <div className={styles.buttonsBox}>
-            {user.role != "admin" && (
+            {user.role !== "admin" && (
               <Button text={"Delete profile"} onClick={handleDelete} />
             )}
             <Button text={"Logout"} onClick={logout} />
