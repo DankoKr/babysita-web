0f9b8bcd205dce09600d9284198a895f59284e3f
Danko Kralski <d.kralski@student.fontys.nl>
2024-02-08 09:52:39 +0100

    Update README + finished project details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,31 @@
-import styles from './Footer.module.css'
+import styles from './Footer.module.css';
 import logo from '../assets/images/BABYSITA.png';
 
 const Footer = () => {
-    return ( 
-        <div className={styles.footer}>
-            <div className={styles['container']}>
-
-                <div className={styles['logo']}>
-                    <img src={logo} alt="Logo" />
-                </div>
-                <div className={styles['column']}>
-                    <h2>Dummy Title</h2>
-                    <p>some dummy text</p>
-                    <p>some dummy text</p>
-                </div>
-                <div className={styles['column']}>
-                    <h2>Dummy Title</h2>
-                    <p>some dummy text</p>
-                    <p>some dummy text</p>
-                </div>
-
-            </div>
-            <br/>
-            <hr />
-            <div className={styles['copyright']}>
-                <p>Copyright © 2023 Babysita</p>
-            </div>
+  return (
+    <div className={styles.footer}>
+      <div className={styles['container']}>
+        <div className={styles['logo']}>
+          <img src={logo} alt='Logo' />
         </div>
-        
-    );
-}
- 
+        <div className={styles['column']}>
+          <h2>Policy</h2>
+          <p>Anyone can join</p>
+          <p>Friendly environment</p>
+        </div>
+        <div className={styles['column']}>
+          <h2>Address</h2>
+          <p>The Netherlands, </p>
+          <p>Fontys Eindhoven</p>
+        </div>
+      </div>
+      <br />
+      <hr />
+      <div className={styles['copyright']}>
+        <p>Copyright © 2023 Babysita</p>
+      </div>
+    </div>
+  );
+};
+
 export default Footer;
